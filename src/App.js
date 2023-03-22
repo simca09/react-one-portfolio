77302812cbccf3f81d99b186374fe71dbceec7ae
simca09 <delacruz.klydean@yahoo.com>
2023-03-22 12:33:03 -0400

    Files changed, home and navbar being fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">Hello</h1>
+      <NavBar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeroImage from "../assets/heroImage.png";
+import {RiArrowRightCircleLine} from 'react-icons/ri'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h2>
+                    
+                </h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
